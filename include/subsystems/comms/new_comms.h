f0493b5fd1677be3f03de562047c15028b4a9dc4
Jaume Cortés <jaume.cort@gmail.com>
2026-02-12 22:25:06 +0100

    Modularized interleaving code

--- include/subsystems/comms/new_comms.h
+++
@@ -1,5 +0,0 @@
-
-
-void new_comms_task(void *pv_parameters);
-void new_setup_comms(void);
-static void new_process_comms(void);
--- src/subsystems/comms/comms.c
+++ src/subsystems/comms/comms.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "interleaving.h"
 
 
 /* ---- Macros and constants ---- */
@@ -167,9 +168,6 @@ void SX1262Config(uint8_t SF, uint8_t CR, uint32_t RF_F);
 
 void TxPrepare(uint8_t messageType);
 
-void Interleave(uint8_t *inputarr, int size);
-
-void Deinterleave(uint8_t *inputarr, int size);
 
 /* ---- Public function definitions ---- */
 
@@ -509,60 +507,4 @@ void TxPrepare(uint8_t messageType) {
     // antes se hacia un memcpy de TxData a un buffer Encoded_Packet. Alomejor es necesario hacerlo asi más adelante
 }
 
-void Interleave(uint8_t *inputarr, int size) 
-{
-    // Check that the size is a multiple of 6.
-    if (size % 6 != 0) {
-        return;
-    }
-
-    int groupSize = size / 6;
-
-    // Allocate temporary array to hold the interleaved result.
-    uint8_t *temp =(uint8_t *) malloc(size * sizeof(uint8_t));
-    if (temp == NULL) {
-        // EXIT_FAILURE no definido
-        //exit(EXIT_FAILURE);
-    }
-
-    // For each index within the groups,
-    // pick one element from each of the 6 groups.
-    for (int i = 0; i < groupSize; i++) {
-        for (int j = 0; j < 6; j++) {
-            temp[i * 6 + j] = inputarr[j * groupSize + i];
-        }
-    }
-
-    // Copy the interleaved elements back into the original array.
-    memcpy(inputarr, temp, size);
-
-    free(temp);
-}
-
-void Deinterleave(uint8_t *inputarr, int size) 
-{
-    if (size % 6 != 0) {
-        return;
-    }
-
-    int groupSize = size / 6;
-    uint8_t *temp =(uint8_t *) malloc(size * sizeof(uint8_t));
-    if (temp == NULL) {
-        // EXIT_FAILURE no definido
-        //exit(EXIT_FAILURE);
-    }
-
-    // Reconstruct the original groups.
-    // For each group index i and for each group j:
-    // The interleaved array holds the jth element of group j at position i*6 + j.
-    // We restore it to temp[ j * groupSize + i ].
-    for (int i = 0; i < groupSize; i++) {
-        for (int j = 0; j < 6; j++) {
-            temp[j * groupSize + i] = inputarr[i * 6 + j];
-        }
-    }
-
-    memcpy(inputarr, temp, size);
-    free(temp);
-}
 
