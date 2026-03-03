f780f8a1e6c9975bf102732b7ed3eb89c294b651
Jaume Cortés <jaume.cort@gmail.com>
2026-03-03 12:09:33 +0100

    comms cleanup

--- src/subsystems/comms/comms.c
+++ src/subsystems/comms/comms.c
@@ -17,10 +17,7 @@
 
 #define TX_OUTPUT_POWER                     18        // dBm
 
-#define LORA_BANDWIDTH                      0         // [0: 125 kHz,
-                                                      //  1: 250 kHz,
-                                                      //  2: 500 kHz,
-                                                      //  3: Reserved]
+#define LORA_BANDWIDTH                      0         // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
 #define LORA_PREAMBLE_LENGTH                8//108    // Same for Tx and Rx
 #define LORA_SYMBOL_TIMEOUT                 100       // Symbols
 #define LORA_FIX_LENGTH_PAYLOAD_ON          0
@@ -70,20 +67,6 @@ static CommsSettings_t CommsSettings = {
     .rxTime = 2000,
     .ackTime = 4000
 };
-
-
-/*!
- * \brief Function configuring CAD parameters
- * \param [in]  cadSymbolNum   The number of symbol to use for CAD operations
- *                             [LORA_CAD_01_SYMBOL, LORA_CAD_02_SYMBOL,
- *                              LORA_CAD_04_SYMBOL, LORA_CAD_08_SYMBOL,
- *                              LORA_CAD_16_SYMBOL]
- * \param [in]  cadDetPeak     Limit for detection of SNR peak used in the CAD
- * \param [in]  cadDetMin      Set the minimum symbol recognition for CAD
- * \param [in]  cadTimeout     Defines the timeout value to abort the CAD activity
- */
-// need to implement RadioLoRaCadSymbols_t
-// void SX126xConfigureCad( RadioLoRaCadSymbols_t cadSymbolNum, uint8_t cadDetPeak, uint8_t cadDetMin , uint32_t cadTimeout);
 
 void TxPrepare(uint8_t messageType);
 
