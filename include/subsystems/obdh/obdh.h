f84bea851165dc27f06d66cbb8abb1e81b87f0b4
Guillermo O'Tuama Pascual <guillermo.o.tuama@estudiantat.upc.edu>
2026-05-30 16:44:49 +0200

    Update obdh module with refactor-obdh branch changes

--- include/subsystems/obdh/obdh.h
+++ include/subsystems/obdh/obdh.h
@@ -13,6 +13,16 @@
 #include "FreeRTOS.h"
 #include "queue.h"
 #include "task.h"
+#include "ht_handling.h"
+
+
+
+#define OBDH_TELEMETRY_PERIOD_MS 10000 //To be defined
+#define MAX_HT_BEACONS 10 //Arbitrary for testing, to be discussed
+#define HT_BEACON_SIZE 16 // following PoCat TM_TC DATABASE, this might change
+#define HT_BASE_ADDR 0x080FE000 //To be defined
+#define HT_POINTER_ADDR 0x080FD000
+
 
 /**
  * @brief Flash operation type.
@@ -43,11 +53,15 @@ typedef struct {
 
 /** @brief Queue used to send flash access requests to the OBDH task. */
 extern QueueHandle_t obdh_queue_handle;
+extern CircularFlashHandler telemetry_handler;
 
 /**
  * @brief OBDH FreeRTOS task entry point.
  * @param pv_parameters Task parameter provided by xTaskCreate(); currently unused.
  */
 void obdh_task(void *pv_parameters);
+void obdh_save_pointers_flash(void);
+HAL_StatusTypeDef obdh_get_telemetry(uint8_t *buffer);
+HAL_StatusTypeDef obdh_insert_telemetry(uint8_t *buffer);
 
 #endif /* INC_OBDH_H_ */
--- src/subsystems/obdh/obdh.c
+++ src/subsystems/obdh/obdh.c
@@ -20,6 +20,7 @@
 
 QueueHandle_t obdh_queue_handle;
 static uint32_t deferred_notifications;
+CircularFlashHandler telemetry_handler;
 
 static void setup_obdh(void);
 static void process_obdh(void);
@@ -37,13 +38,69 @@ void obdh_task(void *pv_parameters) {
 
 }
 
+void obdh_save_pointers_flash(void)
+{
+    Write_Flash(HT_POINTER_ADDR, (uint8_t*)&telemetry_handler, sizeof(CircularFlashHandler));
+}
+
 /**
  * @brief Initialize OBDH task state.
  */
 static void setup_obdh(void) {
     deferred_notifications = 0;
     // Apply the default configuration
+    /*
+    //mirem on ens haviem quedat en memoria. 
+    ht_count=0;
+    ht_head=0;
+    uint32_t prev_epoch=0;
+    uint8_t buf[4]; // First 4 bytes are epoch
 
+    for (uint8_t i=0;i<MAX_HT_BEACONS;i++)
+    {
+        uint32_t addr=HT_BASE_ADDR+(i*HT_BEACON_SIZE);
+        
+        Read_Flash(addr,buf,4);
+        uint32_t curr_epoch=buf[0]<<24 |buf[1]<<16| buf[2]<<8| buf[3] ;
+        if(curr_epoch==0xFFFFFFFF || curr_epoch==0)
+        {
+            ht_head=i;
+            break;//Posició on acaba la cua
+        }
+        else if (curr_epoch < prev_epoch) {
+            
+            ht_head = i;
+            ht_count = MAX_HT_BEACONS; //Hem fet la volta
+            break;
+        }
+
+        else
+        {
+            ht_count++;
+            prev_epoch=curr_epoch;
+        }
+
+    }
+
+*/
+
+    printf("Setting up OBDH...\n");
+    Read_Flash(HT_POINTER_ADDR, (uint8_t*)&telemetry_handler, sizeof(CircularFlashHandler));
+    if(telemetry_handler.flag==telemetry_circular_flag)
+    {
+        printf("telemetria circular creada\n");
+    }
+    else    
+    {
+        printf("telemetria circular no creada\n");
+        telemetry_handler.flag=telemetry_circular_flag;
+        telemetry_handler.current_ht_count=0;
+        telemetry_handler.reading_pointer=0;
+        telemetry_handler.writing_pointer=0;
+        obdh_save_pointers_flash();
+        
+
+    }
 }
 
 /**
@@ -77,18 +134,30 @@ static void process_obdh(void) {
             {
                 Read_Flash(request.addr, request.buf.dst, request.len);
             }
+            /*if(request.client != NULL) {
+                xTaskNotify(request.client, OBC_EVENT_OBDH_DONE, eSetBits);//We send a notification to the task
+                //vTaskDelay(100/portTICK_PERIOD_MS);
+            }*/
             status=HAL_OK;
         }
         else if(request.op == FLASH_WRITE)
+
         {
             if(request.buf.src != NULL)
             {
                 Write_Flash(request.addr, request.buf.src, request.len);
                 status=HAL_OK;
+
+                /*
+                if(request.client != NULL) {
+                    xTaskNotify(request.client, OBC_EVENT_OBDH_DONE, eSetBits);
+                }
+                */
             }
             else
             {
                 status=HAL_ERROR;
+
             }
         }
 
@@ -102,4 +171,47 @@ static void process_obdh(void) {
         }
         
     }
+
+}
+/*
+HAL_StatusTypeDef obdh_get_telemetry(uint8_t *buffer)
+{
+    uint8_t index;
+    uint32_t address;
+    HAL_StatusTypeDef read_state;
+
+    if(ht_count==0)
+    {
+        return HAL_ERROR;
+        
+    }
+    index=(ht_head+MAX_HT_BEACONS-ht_count)%MAX_HT_BEACONS;
+    address=HT_BASE_ADDR+(index*HT_BEACON_SIZE);
+    read_state=OBDH_Read_Request(address,buffer,HT_BEACON_SIZE);
+    if(read_state==HAL_OK)
+    {
+        ht_count--;
+    }
+    return read_state;
+}
+*/
+/*
+HAL_StatusTypeDef obdh_insert_telemetry(uint8_t *buffer)
+{ 
+    uint32_t address;
+    HAL_StatusTypeDef write_state;
+
+    address=HT_BASE_ADDR+(ht_head*HT_BEACON_SIZE);
+    write_state=OBDH_Write_Request(address,buffer,HT_BEACON_SIZE);
+    if (write_state==HAL_OK)
+    {
+        ht_head=(ht_head+1)%HT_BEACON_SIZE;
+        if(ht_head<HT_BEACON_SIZE)
+        {
+            ht_count++;
+        }
+    }
+    return write_state;  
+
+
 }
